9158e1999cd1f1261a541b93366050202f6aa30e
Nicolas Carlier <n.carlier@nunux.org>
2014-09-06 11:45:00 +0000

    Init.

---
+++ README.md
@@ -0,0 +1,62 @@
+cloudconfd
+==========
+
+A very simple cloud-config http server.
+
+This can help when your cloud provider don't provide an easy way to setup user-data.
+
+Installation
+------------
+
+Build cloudconfd:
+
+    go build -o cloudconfd main.go
+
+Or download binary.
+
+Create your own user-data template in the "templates" directory. The filename define the configuration name.
+
+**Example:** "./templates/cloud-config.yaml" will define the "cloud-config" configuration. 
+
+Check this project directory for a sample.
+
+Create a configuration file in a directory named according the configuration name in the 'conf' directory. This file is a YAML file and must be named according the mac address of the node you want to configure. You have to replace ":" char by "_" char (for a better file system compatibility).
+
+**Example:** ./conf/cloud-config/da_0f_f7_b2_a1_b3.yaml
+
+Again, check this project directory for a sample.
+
+And launch the server:
+
+    cloudconfd -l :9090
+
+Now you can fetch the wanted configuration like this:
+
+    curl localhost:9090/cloud-config/da:0f:f7:b2:a1:b3
+
+This will fetch the configuration named "cloud-config" for the MAC address da:0f:f7:b2:a1:b3.
+
+Use case
+--------
+
+A simple use case of cloudconfd is to boot a CoreOS node with a minimalist configdrive configured to link cloud-init with cloudconfd server.
+
+First, create a minimalist user-data setup:
+
+    #!/bin/bash
+    MAC=`ifconfig ens18 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'`
+    URL="http://192.168.0.2:9090/cloud-init/${MAC}"
+    coreos-cloudinit --from-url="${URL}"
+
+Adapt the URL to your cloudconfd server location.
+
+Then create an ISO file:
+
+    mkdir -p /tmp/new-drive/openstack/latest
+    cp user_data /tmp/new-drive/openstack/latest/user_data
+    mkisofs -R -V config-2 -o configdrive.iso /tmp/new-drive
+    rm -r /tmp/new-drive
+
+Mount the iso alongside your CoreOs node and boot the node.
+
+And voila. The CoreOs is set up thanks to cloudconfd.
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+	"net/http"
+	"text/template"
+	"path/filepath"
+	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v1"
+)
+
+var (
+	laddr   = flag.String("l", ":8080", "HTTP service address (e.g.address, ':8080')")
+	templates = template.New("cloudconfd")
+)
+
+type Config struct {
+    Hostname string
+    Ip string
+    Gateway string
+    Ssh_authorized_keys []string
+}
+
+func parseTemplateFiles() {
+	filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			filename := filepath.Base(path)
+			log.Println(fmt.Sprintf("Loading template: %s ...", filename))
+			filetext, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			text := string(filetext)
+			var extension = filepath.Ext(filename)
+			var name = filename[0:len(filename)-len(extension)]
+			template.Must(templates.New(name).Parse(text))
+			log.Println(fmt.Sprintf("Template %s loaded.", name))
+		}
+		return nil
+	})
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
+	mac := params["mac"]
+
+	// open configuration file
+	filename := fmt.Sprintf("./conf/%s/%s.yaml", name, mac)
+	filename = strings.Replace(filename, ":", "_", -1)
+	source, err := ioutil.ReadFile(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// Load YAML configuration
+	var config Config
+	err = yaml.Unmarshal(source, &config)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = templates.ExecuteTemplate(w, name, config)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	// Load templates
+	parseTemplateFiles()
+
+	flag.Parse()
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/{name:[a-z-]+}/{mac:([0-9a-f]{2}([:-]|$)){6}}", handler).Methods("GET")
+
+	http.Handle("/", rtr)
+
+	log.Println("cloudconfd server listening...")
+	log.Fatal(http.ListenAndServe(*laddr, nil))
+}
